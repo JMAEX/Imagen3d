1f00e4c7cf98cf73c3b082452ad31b001fd4e883
JMAEX <jovenm1125@gmail.com>
2022-09-30 12:13:49 -0500

    first commit

---
+++ Index.js
@@ -0,0 +1,19 @@
+
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdddddd)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+let loader = new THREE.GTLFLoader();
+
+
+camera.position.z = 10
+    function animate() {
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera );
+    }
+    animate();
